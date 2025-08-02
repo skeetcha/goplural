35c50dc9f3ee91dfa50dc87d1322f36b565b5484
Cass Unterholzner <cityboundforest@gmail.com>
2025-08-02 13:10:28 -0500

    PluralKit import integration complete

--- pk.go
+++ pk.go
@@ -1,15 +1,22 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"path"
+	"time"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/storage"
 	"fyne.io/fyne/v2/widget"
 )
 
-func PKImport(parent fyne.Window) {
+func PKImport(app fyne.App, parent fyne.Window, memberData *[]Member) {
 	idEntry := widget.NewEntry()
 	warningLabel := widget.NewLabel("WARNING: This will erase all system data to import from PluralKit. Make sure you know what you're doing before you click confirm.")
 	warningLabel.Wrapping = fyne.TextWrapWord
@@ -29,6 +36,112 @@ func PKImport(parent fyne.Window) {
 			return
 		}
 
-		fmt.Println(idEntry.Text)
+		resp, err := http.Get("https://api.pluralkit.me/v2/systems/" + idEntry.Text + "/members")
+
+		if err != nil {
+			log.Println("Error getting system data:", err)
+			return
+		}
+
+		defer resp.Body.Close()
+		var members pkMembers
+
+		err = json.NewDecoder(resp.Body).Decode(&members)
+
+		if err != nil {
+			log.Println("Error parsing system data:", err)
+			return
+		}
+
+		(*memberData) = make([]Member, len(members))
+
+		for i, v := range members {
+			(*memberData)[i].Name = v.Name
+
+			if v.Pronouns != nil {
+				(*memberData)[i].Pronouns = *v.Pronouns
+			}
+
+			if v.Avatar != nil {
+				uri, err := loadAvatar(*v.Avatar, v.Id, app)
+				time.Sleep(1 * time.Second)
+
+				if err == nil {
+					(*memberData)[i].Avatar = uri
+				}
+			}
+
+			(*memberData)[i].Proxy = make([]ProxyTag, len(v.ProxyTags))
+
+			for ip, vp := range v.ProxyTags {
+				(*memberData)[i].Proxy[ip].Prefix = vp.Prefix
+				(*memberData)[i].Proxy[ip].Suffix = vp.Suffix
+			}
+		}
+
+		log.Printf("Imported system %s from PluralKit", idEntry.Text)
 	}, parent)
+}
+
+type pkMembers []pkMember
+
+type pkMember struct {
+	Id        string       `json:"id"`
+	Name      string       `json:"name"`
+	Pronouns  *string      `json:"pronouns,omitempty"`
+	ProxyTags []pkProxyTag `json:"proxy_tags"`
+	Avatar    *string      `json:"avatar_url,omitempty"`
+}
+
+type pkProxyTag struct {
+	Prefix *string `json:"prefix,omitempty"`
+	Suffix *string `json:"suffix,omitempty"`
+}
+
+func loadAvatar(uri string, id string, app fyne.App) (string, error) {
+	log.Printf("Loading avatar %s from %s...", id, uri)
+
+	req, err := http.NewRequest("GET", uri, nil)
+
+	if err != nil {
+		log.Println("Error making request:", err)
+		return "", err
+	}
+
+	req.Header.Set("User-Agent", "Mozilla/5.0")
+
+	resp, err := new(http.Client).Do(req)
+
+	if err != nil {
+		log.Println("Error downloading avatar:", err)
+		return "", err
+	}
+
+	fmt.Println(resp.Status)
+	defer resp.Body.Close()
+	data, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		log.Println("Error reading avatar data:", err)
+		return "", err
+	}
+
+	imageURI := storage.NewFileURI(path.Join(app.Storage().RootURI().Path(), id+path.Ext(uri)))
+	f, err := storage.Writer(imageURI)
+
+	if err != nil {
+		log.Println("Error opening file for writing:", err)
+		return "", err
+	}
+
+	defer f.Close()
+	val, err := f.Write(data)
+
+	if err != nil {
+		log.Println("Error writing file:", err)
+		return "", err
+	}
+
+	fmt.Println(val)
+	return imageURI.Name(), nil
 }
