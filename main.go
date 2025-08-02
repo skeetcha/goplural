82a93540f0a8c6923707823796f75042ddd627ab
Cass Unterholzner <cityboundforest@gmail.com>
2025-08-01 22:01:53 -0500

    Adding basic fyne stuff

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"github.com/skeetcha/goplural/themes"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
 )
 
 type GoPlural struct {
@@ -12,4 +15,24 @@ func main() {
 	appData := GoPlural{
 		themes: themes.SetupThemes(),
 	}
+
+	a := app.New()
+	w := a.NewWindow("Hello World")
+	w.Resize(fyne.NewSize(800, 600))
+	w.SetMainMenu(makeMenu(a, w))
+	w.Show()
+	a.Run()
+}
+
+func makeMenu(app fyne.App, window fyne.Window) *fyne.MainMenu {
+	newItem := fyne.NewMenuItem("New", nil)
+
+	file := fyne.NewMenu("File", newItem)
+
+	main := fyne.NewMainMenu(
+		file,
+		// something else, presumably
+	)
+
+	return main
 }
